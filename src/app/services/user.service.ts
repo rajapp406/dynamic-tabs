b09cca1ff5611ecc6b8aa7afa5f34bc763d67bf0
rajapp406 <raj.lh404@gmail.com>
2021-01-06 17:59:49 +0530

    3 files modified

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,19 +1,20 @@
-import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
-import { BehaviorSubject, Observable, Subject } from 'rxjs';
-import { environment } from 'src/environments/environment';
-import { IUserLogin, UserLogin } from '../types/user.type';
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { BehaviorSubject, Observable } from "rxjs";
+import { environment } from "../../environments/environment";
+import { IUserLogin, UserLogin } from "../types/user.type";
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: "root"
 })
 export class UserService {
-public userLogin$ = new BehaviorSubject<UserLogin>({type: "NOT_LOGGEDIN",  data: true});
-private userLogin = this.userLogin$.asObservable();
-  constructor(
-    private http: HttpClient
-  ) { }
-  loginUser(payload: IUserLogin): Observable<any>{
+  public userLogin$ = new BehaviorSubject<UserLogin>({
+    type: "NOT_LOGGEDIN",
+    data: true
+  });
+  private userLogin = this.userLogin$.asObservable();
+  constructor(private http: HttpClient) {}
+  loginUser(payload: IUserLogin): Observable<any> {
     return this.http.post(`${environment.endPoints.baseUri}/posts`, payload);
   }
 }
